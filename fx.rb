df771415b9929adeaed18a7d3355ddb1ff6fe510
jathu <jathu.satkunarajah@gmail.com>
2022-02-08 17:54:27 -0800

    create generator

--- fx.rb
+++ fx.rb
@@ -1,11 +1,8 @@
-FX_VERSION = "0.1"
-FX_SHA256 = "fe84dd96419c0417890366df59be11852c5bd0f93750cb8c37ab8c7892d8c4d0"
-
 class Fx < Formula
   desc "fx is a workspace tool manager. It allows you to create consistent, discoverable, language-neutral and developer friendly command line tools."
   homepage "https://jathu.me/fx"
-  url "https://github.com/jathu/fx/releases/download/#{FX_VERSION}/fx-#{FX_VERSION}-macos-x86.tar"
-  sha256 FX_SHA256
+  url "https://github.com/jathu/fx/releases/download/0.1/fx-0.1-macos-x86.tar"
+  sha256 "fe84dd96419c0417890366df59be11852c5bd0f93750cb8c37ab8c7892d8c4d0"
   license "Apache-2.0"
 
   def install
@@ -13,6 +10,6 @@ class Fx < Formula
   end
 
   test do
-    assert_match("fx-#{FX_VERSION}", shell_output("#{bin}/fx version"))
+    assert_match("fx-0.1", shell_output("#{bin}/fx version"))
   end
 end
